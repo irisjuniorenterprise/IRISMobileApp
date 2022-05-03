743471388cf7954ec9162ef832b79e8be825fbdf
Khaled LAJILI <khaled.lajili@enis.tn>
2022-05-04 00:24:00 +0100

    feat ( multiAppsConfig ) : change the Kernel - add the app name parameter to the constructor - change the cache directory based on app name - change the log directory base on app name - load routes base on app name

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,8 +4,39 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+    private String $name;
+
+    public function __construct($environment, $debug, $name)
+    {
+        $this->name = $name;
+
+        parent::__construct($environment, $debug);
+    }
+
+    public function getCacheDir(): string
+    {
+        return $this->getProjectDir().'/var/cache/'.$this->name.'/'.$this->environment;
+    }
+
+    public function getLogDir(): string
+    {
+        return $this->getProjectDir().'/var/log/'.$this->name;
+    }
+
+    protected function configureRoutes(RoutingConfigurator $routes): void
+    {
+        $confDir = $this->getProjectDir().'/config/'.$this->name;
+        if (is_dir($confDir.'/routes/')) {
+            $routes->import($confDir.'/routes/*.yaml');
+        }
+        if (is_dir($confDir.'/routes/'.$this->environment)) {
+            $routes->import($confDir.'/routes/'.$this->environment.'/**/*.yaml');
+        }
+        $routes->import('../config/'.$this->name.'/routes.yaml');
+    }
 }
